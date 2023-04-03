b1b340a10a08114a8ca58cf5e0253584060790c4
txr-nicole <nicolefagundes24@gmail.com>
2023-04-03 16:00:20 -0300

    💄 add Banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,8 @@
+import "./Banner.css";
+export function Banner() {
+  return (
+      <header className="banner">
+        <img src="/imagens/banner.png" alt="banner principal" />
+      </header>
+  );
+}
