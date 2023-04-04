46c8512ce57c38106f44dccd170535fde8f735dd
txr-nicole <nicolefagundes24@gmail.com>
2023-04-04 14:55:37 -0300

    💄 add ListaSuspensa component to form

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,8 +1,19 @@
 import { CampoTexto } from "../CampoTexto";
+import { ListaSuspensa } from "../ListaSuspensa";
 import "./Formulario.css";
 
 
 export const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+    ]
+  
   return (
     <section className="formulario">
       <form>
@@ -10,6 +21,7 @@ export const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Times"  itens={times}/>
       </form>
     </section>
   );
