2f1681490fd1defa8090ab083128f85c1905de19
txr-nicole <nicolefagundes24@gmail.com>
2023-04-04 16:20:55 -0300

    🦺 add required on inputs

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -15,14 +15,19 @@ export const Formulario = () => {
     "Inovação e Gestão",
     ]
   
+  const aoSalvar = (e) => {
+    e.preventDefault();
+    console.log("form foi submitado")
+  }
+  
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Time"  itens={times}/>
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto  label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa obrigatorio={true} label="Time"  itens={times}/>
         <Botao>
           Criar card
         </Botao>
--- src/components/ListaSuspensa/index.js
+++ src/components/ListaSuspensa/index.js
@@ -4,7 +4,7 @@ export const ListaSuspensa = (props) => {
   return (
     <div className="lista-suspensa">
         <label>{props.label}</label>
-        <select>
+        <select required={props.obrigatorio}>
             {props.itens?.map((item) => <option key={item}>{item}</option>)}
         </select>
     </div>
