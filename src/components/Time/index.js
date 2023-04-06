eaf0b27b2667cbc5acf80801cbe97bc228ff58b4
txr-nicole <nicolefagundes24@gmail.com>
2023-04-06 19:59:17 -0300

    ✨ adds colaborators to Time component

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,9 +1,22 @@
-import "./Time.css"
+import { Colaborador } from "../Colaborador";
+import "./Time.css";
 
-export const Time = (props)=>{
-    return (
-      <section style={{ backgroundColor: props.corSecundaria }} className="time">
-        <h3 style={{borderBottomColor: props.corPrimaria}}>{props.nome}</h3>
-      </section>
-    );
-}
+export const Time = (props) => {
+  return (
+  props.colaboradores.length > 0 &&
+    <section style={{ backgroundColor: props.corSecundaria }} className="time">
+      <h3 style={{ borderBottomColor: props.corPrimaria }}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+            corPrimaria={props.corPrimaria}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
