854420add93f51803312dae1786f9e655483eb09
txr-nicole <nicolefagundes24@gmail.com>
2023-04-06 18:45:29 -0300

    💄 adds Colaborador component

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,21 @@
+import "./Colaborador.css";
+
+export const Colaborador = ({nome,imagem,cargo, corPrimaria})=>{
+    return (
+      <div className="colaborador">
+        <div
+          className="cabecalho"
+          style={{ backgroundColor: corPrimaria }}
+        >
+          <img
+            src={imagem}
+            alt="Foto do colaborador"
+          />
+        </div>
+        <div className="rodape">
+          <h4>{nome}</h4>
+          <h5>{cargo}</h5>
+        </div>
+      </div>
+    );
+}
